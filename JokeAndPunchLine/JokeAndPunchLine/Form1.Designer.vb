bb2d4131931a1eae4036eb8ca9368783e0c23a9f
ZinatAfrooz <S01012776@acad.tri-c.edu>
2018-01-29 21:39:24 -0500

    change

--- JokeAndPunchLine/JokeAndPunchLine/Form1.Designer.vb
+++ JokeAndPunchLine/JokeAndPunchLine/Form1.Designer.vb
@@ -57,7 +57,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(370, 261)
+        Me.ClientSize = New System.Drawing.Size(379, 261)
         Me.Controls.Add(Me.btnPunchLine)
         Me.Controls.Add(Me.btnSetup)
         Me.Controls.Add(Me.lblMessage)
